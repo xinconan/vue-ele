fb815f0ec167016fe42501422bcfab91289cc117
xinconan <xinconan2@sina.com>
2017-02-07 20:18:10 +0800

    弹出层标题

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -40,6 +40,11 @@
             <div class="star-wrapper">
               <star :size="48" :score="seller.score"></star>
             </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
           </div>
         </div>
         <div class="detail-close" @click="hideDetail">
@@ -268,6 +273,24 @@
             margin-top: 16px;
             padding: 2px 0;
             text-align: center;
+          }
+          .title{
+            display: flex;
+            width: 80%;
+            margin: 28px auto 24px auto;
+
+            .line{
+              flex: 1;
+              position: relative;
+              top: -6px;
+              border-bottom: 1px solid rgba(255,255,255,.2);
+            }
+
+            .text{
+              padding: 0 12px;
+              font-weight: 700;
+              font-size: 14px;
+            }
           }
         }
       }
