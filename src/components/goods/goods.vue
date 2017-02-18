e30380b50702da79a5b760901db49f25231dd88e
xinconan <xinconan2@sina.com>
2017-02-18 15:47:58 +0800

    goods排版

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="food in item.foods" class="food-item">
               <div class="icon">
-                <img :src="food.icon">
+                <img width="57" height="57" :src="food.icon">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
@@ -25,7 +25,7 @@
                   <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">¥{{food.price}}</span><span class="old">¥{{food.oldPrice}}</span>
+                  <span class="now">¥{{food.price}}</span><span class="old" v-if="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -121,6 +121,67 @@
     // 右菜单
     .foods-wrapper{
       flex: 1;
+
+      .title{
+        padding-left: 14px;
+        line-height: 26px;
+        height: 26px;
+        border-left: 2px solid #d9dde1;
+        background: #f3f5f7;
+        font-size: 12px;
+        color: rgb(147, 153, 159);
+      }
+      .food-item{
+        display: flex;
+        margin: 18px;
+        padding-bottom: 18px;
+        @include border-bottom(rgba(7, 17, 27, 0.1));
+        &:last-child{
+          margin-bottom: 0;
+        }
+        .icon{
+          flex: 0 0 57px;
+          margin-right: 10px;
+        }
+        .content{
+          flex: 1;
+          .name{
+            margin: 2px 0 8px 0;
+            height: 14px;
+            line-height: 14px;
+            font-size: 14px;
+            color: rgb(7, 17, 27);
+          }
+          .desc, .extra{
+            line-height: 10px;
+            font-size: 10px;
+            color: rgb(147, 153, 159);
+          }
+          .desc{
+            line-height: 12px;
+            margin-bottom: 8px;
+          }
+          .extra{
+            .count{
+              margin-right: 12px;
+            }
+          }
+          .price{
+            font-weight: 700;
+            line-height: 24px;
+            .now{
+              margin-right: 8px;
+              font-size: 14px;
+              color: rgb(240, 20, 20);
+            }
+            .old{
+              text-decoration: line-through;
+              font-size: 10px;
+              color: rgb(147, 153, 159);
+            }
+          }
+        }
+      }
     }
   }
 </style>
